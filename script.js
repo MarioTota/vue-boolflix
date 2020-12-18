de21b1dc9108ce102470969546a091fd646735a9
Mario <mario.tota.mt@gmail.com>
2020-12-19 00:30:54 +0100

    commit

--- script.js
+++ script.js
@@ -1,27 +1,39 @@
 var app = new Vue({
-  el: '#root',
-  data: {
-    films: [],
-    images: [],
-    prefix: 'https://image.tmdb.org/t/p/w220_and_h330_face'
+    el: '#root',
+    data: {
+      films: [],
+      prefix: 'https://image.tmdb.org/t/p/w220_and_h330_face',
+      message: ''
+  	},
+    methods: {
+      pippo: function () {
+          console.log("ciao");
+          axios
+            .get('https://api.themoviedb.org/3/search/movie?api_key=45461509edc8d818f3b7e4e72e2b03e5&language=it-IT&query='+ this.message +'&page=1&include_adult=false')
+            .then((result) =>  {
+              result.data.results.forEach(
+                (element) => {
+                  this.films.push(element);
+                }
+              )
+            });
+        }
+    },
 
-	},
-  methods: {
+    // mounted:
+    //   function () {
+    //     console.log("ciao");
+    //     axios
+    //       .get('https://api.themoviedb.org/3/trending/all/day?api_key=45461509edc8d818f3b7e4e72e2b03e5')
+    //       .then((result) =>  {
+    //         result.data.results.forEach(
+    //           (element) => {
+    //             this.trending.push(element);
+    //           }
+    //         )
+    //         console.log(this.trending);
+    //       });
+    //   }
 
-  },
-  mounted:
-    function () {
-      var self = this;
-      axios
-        .get('https://api.themoviedb.org/3/search/movie?api_key=45461509edc8d818f3b7e4e72e2b03e5&language=it-IT&query=sniper&page=1&include_adult=false')
-        .then(function(result)  {
-          result.data.results.forEach(
-            (element) => {
-              self.films.push(element)
-              self.images.push('https://image.tmdb.org/t/p/w220_and_h330_face' + element.poster_path)
-            }
-          )
-          console.log(self.images);
-        });
-    }
+
 });
