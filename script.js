a6c2d43a939d2b7ba54a23e5228a35dbd9ad3f9e
Mario <mario.tota.mt@gmail.com>
2020-12-18 21:09:03 +0100

    commit

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var app = new Vue({
   el: '#root',
   data: {
     films: [],
-    filmCercato:''
+    linkImages: []
 	},
   methods: {
 
@@ -11,15 +11,15 @@ var app = new Vue({
     function () {
       var self = this;
       axios
-        .get('https://api.themoviedb.org/3/search/movie?api_key=45461509edc8d818f3b7e4e72e2b03e5&language=it-IT&query=ciao&page=1&include_adult=false')
+        .get('https://api.themoviedb.org/3/search/movie?api_key=45461509edc8d818f3b7e4e72e2b03e5&language=it-IT&query=sniper&page=1&include_adult=false')
         .then(function(result)  {
           result.data.results.forEach(
             (element) => {
               self.films.push(element)
+              self.images.push('https://image.tmdb.org/t/p/w220_and_h330_face' + element.poster_path)
             }
           )
-          console.log(self.films);
-
+          console.log(self.images);
         });
     }
 });
